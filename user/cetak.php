8835ffad337816cbacb66b318b91e01a0be85917
langgs17 <anggita030321@gmail.com>
2024-11-15 10:26:57 +0700

    DONEE

--- user/cetak.php
+++ user/cetak.php
@@ -34,7 +34,6 @@ if ($pemesanan) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cetak Tiket</title>
     <style>
         /* CSS untuk tampilan tiket */
         .ticket-container {
