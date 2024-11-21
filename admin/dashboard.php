6eae1b5b3d4f9d9e3381a1b046cc970f28751875
langgs17 <anggita030321@gmail.com>
2024-11-21 20:11:02 +0700

    gatau

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -34,42 +34,148 @@
           </div>
         </div>
           <div class="row">
-            <div class="col-xl-3 col-md-6">
-                <div class="card shadow-none bg-transparent border border-primary mb-3">
-                    <div class="card-body">Data Kereta</div>
-                    <div class="card-footer d-flex align-items-center justify-content-between">
-                        <a class="small text-black stretched-link" href="index.php?page=kereta">Lihat Detail</a>
-                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+            <div class="col-lg-3 col-md-12 col-6 mb-4">
+              <?php
+                $kereta = "SELECT COUNT(*) AS total_kereta FROM kereta";
+                $total_kereta = $mysqli->query($kereta);
+                if ($total_kereta->num_rows > 0) {
+                  $row = $total_kereta->fetch_assoc();
+                }
+              ?>
+                <div class="card">
+                  <div class="card-body">
+                    <div class="card-title d-flex align-items-start justify-content-between">
+                      <div class="avatar flex-shrink-0">
+                        <i class="bx bx-train text-primary" style="font-size: 2rem; background-color: #e9f7ef; padding: 10px; border-radius: 50%;"></i>
+                      </div>
+                      <div class="dropdown">
+                        <button
+                          class="btn p-0"
+                          type="button"
+                          id="cardOpt3"
+                          data-bs-toggle="dropdown"
+                          aria-haspopup="true"
+                          aria-expanded="false"
+                        >
+                          <i class="bx bx-dots-vertical-rounded"></i>
+                        </button>
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt3">
+                          <a class="dropdown-item" href="?page=kereta">Detail</a>
+                        </div>
+                      </div>
                     </div>
+                    <span class="fw-semibold d-block mb-1">Kereta Tersedia</span>
+                    <h2 class="card-title mb-2"><?php echo $row['total_kereta'];?></h2>
+                  </div>
                 </div>
-            </div>
-            <div class="col-xl-3 col-md-6">
-                <div class="card shadow-none bg-transparent border border-warning mb-3">
-                    <div class="card-body">Data Stasiun</div>
-                    <div class="card-footer d-flex align-items-center justify-content-between">
-                        <a class="small text-black stretched-link" href="?page=stasiun">Lihat Detail</a>
-                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+              </div>
+              <div class="col-lg-3 col-md-12 col-6 mb-4">
+              <?php
+                $stasiun = "SELECT COUNT(*) AS total_stasiun FROM stasiun";
+                $total_stasiun = $mysqli->query($stasiun);
+                if ($total_stasiun->num_rows > 0) {
+                  $row = $total_stasiun->fetch_assoc();
+                }
+              ?>
+                <div class="card">
+                  <div class="card-body">
+                    <div class="card-title d-flex align-items-start justify-content-between">
+                      <div class="avatar flex-shrink-0">
+                        <i class="bx bx-arch text-warning" style="font-size: 2rem; background-color: #FFF5DC; padding: 10px; border-radius: 50%;"></i>
+                      </div>
+                      <div class="dropdown">
+                        <button
+                          class="btn p-0"
+                          type="button"
+                          id="cardOpt3"
+                          data-bs-toggle="dropdown"
+                          aria-haspopup="true"
+                          aria-expanded="false"
+                        >
+                          <i class="bx bx-dots-vertical-rounded"></i>
+                        </button>
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt3">
+                          <a class="dropdown-item" href="?page=kereta">Detail</a>
+                        </div>
+                      </div>
                     </div>
+                    <span class="fw-semibold d-block mb-1">Data Stasiun</span>
+                    <h2 class="card-title mb-2"><?php echo $row['total_stasiun'];?></h2>
+                  </div>
                 </div>
-            </div>
-            <div class="col-xl-3 col-md-6">
-                <div class="card shadow-none bg-transparent border border-success mb-3">
-                    <div class="card-body">Data Route</div>
-                    <div class="card-footer d-flex align-items-center justify-content-between">
-                        <a class="small text-black stretched-link" href="?page=route">Lihat Detail</a>
-                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+              </div>
+              <div class="col-lg-3 col-md-12 col-6 mb-4">
+              <?php
+                $route = "SELECT COUNT(*) AS total_route FROM route";
+                $total_route = $mysqli->query($route);
+                if ($total_route->num_rows > 0) {
+                  $row = $total_route->fetch_assoc();
+                }
+              ?>
+                <div class="card">
+                  <div class="card-body">
+                    <div class="card-title d-flex align-items-start justify-content-between">
+                      <div class="avatar flex-shrink-0">
+                        <i class="bx bx-trip text-success" style="font-size: 2rem; background-color: #e9f7ef; padding: 10px; border-radius: 50%;"></i>
+                      </div>
+                      <div class="dropdown">
+                        <button
+                          class="btn p-0"
+                          type="button"
+                          id="cardOpt3"
+                          data-bs-toggle="dropdown"
+                          aria-haspopup="true"
+                          aria-expanded="false"
+                        >
+                          <i class="bx bx-dots-vertical-rounded"></i>
+                        </button>
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt3">
+                          <a class="dropdown-item" href="?page=kereta">Detail</a>
+                        </div>
+                      </div>
                     </div>
+                    <span class="fw-semibold d-block mb-1">Data Route</span>
+                    <h2 class="card-title mb-2"><?php echo $row['total_route'];?></h2>
+                  </div>
                 </div>
-            </div>
-            <div class="col-xl-3 col-md-6">
-                <div class="card shadow-none bg-transparent border border-info mb-3">
-                    <div class="card-body">Konfirmasi Pembayaran</div>
-                    <div class="card-footer d-flex align-items-center justify-content-between">
-                        <a class="small text-black stretched-link" href="?page=pemesanan_pending">Lihat Detail</a>
-                        <div class="small text-black"><i class="fas fa-angle-right"></i></div>
+              </div>
+              <div class="col-lg-3 col-md-12 col-6 mb-4">
+              <?php
+                $konfirm = "SELECT COUNT(*) AS total_konfirm FROM pemesanan WHERE status='pending'";
+                $total_konfirm = $mysqli->query($konfirm);
+                if ($total_konfirm->num_rows > 0) {
+                  $row = $total_konfirm->fetch_assoc();
+                }
+              ?>
+                <div class="card">
+                  <div class="card-body">
+                    <div class="card-title d-flex align-items-start justify-content-between">
+                      <div class="avatar flex-shrink-0">
+                        <i class="bx bx-wallet-alt text-info" style="font-size: 2rem; background-color: #e9f7ef; padding: 10px; border-radius: 50%;"></i>
+                      </div>
+                      <div class="dropdown">
+                        <button
+                          class="btn p-0"
+                          type="button"
+                          id="cardOpt3"
+                          data-bs-toggle="dropdown"
+                          aria-haspopup="true"
+                          aria-expanded="false"
+                        >
+                          <i class="bx bx-dots-vertical-rounded"></i>
+                        </button>
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="cardOpt3">
+                          <a class="dropdown-item" href="?page=kereta">Detail</a>
+                        </div>
+                      </div>
                     </div>
+                    <span class="fw-semibold d-block mb-1">Konfirmasi Pembayaran</span>
+                    <h2 class="card-title mb-2"><?php echo $row['total_konfirm'];?></h2>
+                  </div>
                 </div>
-            </div>
+              </div>
+
+              
         </div>
 
     </div>
